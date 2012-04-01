78e8e5e1a8390df07365c8382553bfb2a54f1508
Joe Colosimo <jwc@jwcxz.com>
2012-04-01 12:22:23 -0400

    initial commit

---
+++ irc2sms.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python2
+
+import argparse, time, yaml, zmq
+from googlevoice import Voice
+
+p = argparse.ArgumentParser(description="irc2sms");
+p.add_argument('-i', '--ignore', metavar='NICK', default='', nargs='+', help='nicks to ignore');
+p.add_argument('-s', '--source', default='tcp://*:2428', help='ZeroMQ source');
+p.add_argument('-d', '--destination', required=True, help='phone number to send sms to');
+args = p.parse_args();
+
+print "Connecting to Google Voice... ",
+voice = Voice();
+voice.login();
+print "done!"
+
+context = zmq.Context();
+ 
+subscriber = context.socket(zmq.SUB);
+subscriber.connect(args.source);
+subscriber.setsockopt(zmq.SUBSCRIBE, "");
+
+while True:
+    try:
+        rx = subscriber.recv();
+
+        # lolololol this is dumb -- it seems to be a difference between the
+        # ruby implementation of ZeroMQ and the Python implementation
+        rx = rx.replace('!binary', '!!binary');
+
+        data = yaml.load(rx);
+
+        if data[':away'] and \
+                len([ _ for _ in args.ignore if 'nick_'+_ in data[':tags'] ]) == 0:
+            # status was away and message wasn't from an ignored nick, so it's
+            # safe to send
+            if data[':type'] == 'private':
+                msg = "%s: %s" % (data[':channel'], data[':message']);
+            else:
+                sender = "";
+                for _ in data[':tags']:
+                    if _[0:5] == 'nick_':
+                        sender = _[5:];
+                msg = "[%s %s] %s: %s" % (data['server'], data['channel'],
+                        sender, data['message']);
+
+            voice.send_sms(args.destination, msg);
+            print "Message from %s" % msg.split(':')[0];
+
+    except:
+        print "Error!"
